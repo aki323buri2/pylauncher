95d13420d633f708f578a48701458494e8fa933c
a-nakajima <a-nakajia@shokuryu.co.jp>
2019-08-12 14:10:51 +0900

    activate.bat

--- activate.bat
+++ activate.bat
@@ -1,32 +1,38 @@
 @echo off
-set PYTHON=%CD%\python-3.7.4-embed-amd64
-set VENV=%CD%\.VENV
-set PREFIX=%PYTHON%
-set PREFIX=%PREFIX%;%VENV%\Lib\site-packages\pywin32_system32
+set ROOT=.
+set PYTHON=python-3.7.4-embed-amd64
+set VENV=.venv
+echo.
+set PREFIX=%CD%\%ROOT%\%PYTHON%
 set PATH=%PREFIX%;%PATH%
-echo ''
-echo ##############################################
-echo ##############################################
-echo # 
-echo # * Python実行環境構築
-echo # 
-echo ##############################################
-echo ##############################################
-echo .
-echo 以下のパスをPATH環境変数へ追加追加しました : 
-echo .
-echo %PYTHON%
-echo %VENV%\Lib\site-packages\pywin32_system32
-echo .
-
-set PYTHONPATHS=Python37._pth
-copy %PYTHONPATHS% %PYTHON%\
-echo %PYTHONPATHS% を %PYTHON% にコピーしました
-echo ##############################################
-echo ##############################################
-echo # 
-echo .
-echo # * Pythonの実行環境が整いました
-echo # 
-echo ##############################################
-echo ##############################################
+echo %PREFIX% をPATH環境変数に追加しました
+echo.
+set PREFIX=%CD%\%ROOT%\%VENV%
+set PATH=%PREFIX%;%PATH%
+echo %PREFIX% をPATH環境変数に追加しました
+echo.
+echo python.exe の参照順 : 
+where python
+echo.
+set PYTHON_PATHS_CONFIG=python37._pth
+set FILE=%PYTHON_PATHS_CONFIG%
+type nul> %FILE%
+echo python37.zip>> %FILE%
+echo .>> %FILE%
+echo %CD%\%ROOT%\%VENV%\Scripts>> %FILE%
+echo %CD%\%ROOT%\%VENV%\Lib\site-packages>> %FILE%
+echo %CD%\%ROOT%\%VENV%\Lib\site-packages\win32>> %FILE%
+echo %CD%\%ROOT%\%VENV%\Lib\site-packages\win32\Lib>> %FILE%
+echo # Uncomment to run site.main() automatically>> %FILE%
+echo # import site>> %FILE%
+echo ######################################
+echo [%FILE%]
+echo --------------------------------------
+type %FILE%
+echo ######################################
+echo.
+set SRC=%CD%\%PYTHON_PATHS_CONFIG%
+set TAR=%CD%\%ROOT%\%PYTHON%\%PYTHON_PATHS_CONFIG%
+echo コピー元 : %SRC%
+echo コピー先 : %TAR%
+copy %SRC% %TAR%
